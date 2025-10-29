550a1ae2bfb6bb84e5b3d009b076fc7497e33ade
ra1z <purnamaanrai7@gmail.com>
2025-10-29 08:06:08 +0545

    Initial Commit

---
+++ BubbleSort/BubbleSortApp.swift
@@ -0,0 +1,17 @@
+//
+//  BubbleSortApp.swift
+//  BubbleSort
+//
+//  Created by Purnaman Rai (College) on 29/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct BubbleSortApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BubbleSort/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BubbleSort
+//
+//  Created by Purnaman Rai (College) on 29/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
